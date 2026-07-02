25f926173bfdd745b9eece3c3792e6bc3598ab5b
Bugi <alexander.bucksteg@gmail.com>
2026-07-02 17:44:29 +0200

    Service Init

--- src/main/java/com/bucksteg/mediumtracker/service/GameService.java
+++ src/main/java/com/bucksteg/mediumtracker/service/GameService.java
@@ -1,5 +1,32 @@
 package com.bucksteg.mediumtracker.service;
 
+import org.springframework.stereotype.Service;
+
+import com.bucksteg.mediumtracker.entity.Game;
+import com.bucksteg.mediumtracker.exception.GameNotFoundException;
+import com.bucksteg.mediumtracker.repository.GameRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j      //Logger for info
+@Service
 public class GameService {
-    
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) { //Dependency Injection
+        this.gameRepository = gameRepository;
+    }
+
+    public Game getGame(Long id) {
+        return gameRepository
+        .findById(id)
+        .orElseThrow(() -> new GameNotFoundException(id));
+    }
+
+    public void printGame(Long id) {
+        Game game = getGame(id);
+
+        log.info("Found game: {}", game.getTitle());            //Logger setzt den Wert in {} ein, wenn dieser existiert
+    }
 }
--- src/main/java/com/bucksteg/mediumtracker/service/MovieService.java
+++ src/main/java/com/bucksteg/mediumtracker/service/MovieService.java
@@ -1,5 +1,32 @@
 package com.bucksteg.mediumtracker.service;
 
+import org.springframework.stereotype.Service;
+
+import com.bucksteg.mediumtracker.entity.Movie;
+import com.bucksteg.mediumtracker.exception.GameNotFoundException;
+import com.bucksteg.mediumtracker.repository.MovieRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j      //Logger for info
+@Service
 public class MovieService {
-    
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) { //Dependency Injection
+        this.movieRepository = movieRepository;
+    }
+
+    public Movie getMovie(Long id) {
+        return movieRepository
+        .findById(id)
+        .orElseThrow(() -> new GameNotFoundException(id));
+    }
+
+    public void printGame(Long id) {
+        Movie movie = getMovie(id);
+
+        log.info("Found game: {}", movie.getTitle());            //Logger setzt den Wert in {} ein, wenn dieser existiert
+    }
 }
